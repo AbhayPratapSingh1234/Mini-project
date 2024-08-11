94ce8ebfa404cce01ad996b29247caa677612596
AbhayPratapSingh1234 <ap8431815@gmail.com>
2024-08-11 19:08:10 +0530

    nothing

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelector(".close-button").addEventListener("click",function(){
+    document.querySelector(".wrapper").style.display="none";
+})
+document.querySelector(".btn").addEventListener("click",function(){
+    document.querySelector(".wrapper").style.display=" ";
+    
+})
+document.querySelector("#log-btn").addEventListener("click",function(){
+    document.querySelector(".wrapper").style.display="";
+})
